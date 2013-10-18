2c0c6b51292b8ccb0793e9bbbcb78ff49c92baac
Christian Gärtner <christiangaertner.film@googlemail.com>
2013-10-18 15:40:29 +0200

    Adding classes for handling injector method detection

---
+++ src/DaGardner/DaContainer/InjectorDetection/DetectorInterface.php
@@ -0,0 +1,20 @@
+<?php namespace DaGardner\DaContainer\InjectorDetection;
+/*
+ *  (c) Christian Gärtner <christiangaertner.film@googlemail.com>
+ * This file is part of the DaContainer package
+ * License: View distributed LICENSE file
+ */
+
+/**
+* An interfaces for classes which can detect dependency injector methods.
+* @author Christian Gärtner <christiangaertner.film@googlemail.com>
+*/
+interface DetectorInterface
+{
+	/**
+	 * Detect if the given method is injector method
+	 * @param  ReflectionMethod $name The method to check
+	 * @return bool                   Whether the method is injector method.
+	 */
+    public function detect(ReflectionMethod $name);
+}
---
+++ src/DaGardner/DaContainer/InjectorDetection/SimpleDetector.php
@@ -0,0 +1,26 @@
+<?php namespace DaGardner\DaContainer\InjectorDetection;
+/*
+ *  (c) Christian Gärtner <christiangaertner.film@googlemail.com>
+ * This file is part of the DaContainer package
+ * License: View distributed LICENSE file
+ */
+
+use InvalidArgumentException;
+
+/**
+* This class handels a simple the injector-method detection
+* (If the name starts with "set")
+* @author Christian Gärtner <christiangaertner.film@googlemail.com>
+*/
+class SimpleDetector implements DetectorInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function detect(ReflectionMethod $name)
+    {
+        return (strpos($method->name, 'set') === 0);
+    }
+
+}
